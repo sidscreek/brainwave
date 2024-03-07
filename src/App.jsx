4c9eb43bc020c210cd6059a0dd5d166077c2443d
SiddhantJain <siddhantjain027@gmail.com>
2024-03-08 02:49:51 +0530

    changes made (1)

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,13 @@
-function App() {
+import ButtonGradient from "./assets/svg/ButtonGradient";
+import Header from "./components/Header";
+const App = () => {
   return (
-    <h1 className="text-3xl font-bold underline">Hello world!, maa chudao</h1>
+    <>
+      <div className="pt-[4.5rem] lg:pt-[5.25rem] overflow-hidden">
+        <Header />
+      </div>
+      <ButtonGradient />
+    </>
   );
-}
+};
 export default App;
