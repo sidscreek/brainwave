f35e8175335146c01794198d363ca2417a9bc3a3
SiddhantJain <siddhantjain027@gmail.com>
2024-03-10 08:03:32 +0530

    collaboration+chart

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,6 @@
 import ButtonGradient from "./assets/svg/ButtonGradient";
+import Benefits from "./components/Benefits";
+import Collaboration from "./components/Collaboration";
 import Header from "./components/Header";
 import Hero from "./components/Hero";
 const App = () => {
@@ -7,6 +9,8 @@ const App = () => {
       <div className="pt-[4.5rem] lg:pt-[5.25rem] overflow-hidden">
         <Header />
         <Hero />
+        <Benefits />
+        <Collaboration />
       </div>
       <ButtonGradient />
     </>
